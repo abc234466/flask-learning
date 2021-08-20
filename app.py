f68128cf7ce4eb55c3c511d3e84afa64f8e37859
Percy Chen <abc2344666@gmail.com>
2021-08-20 10:36:58 +0800

    fix: url escape

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, url_for
+from markupsafe import escape
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route('/hello/<username>')
+def hello_user(username):
+    return f"Hello {escape(username)}!"
+
+with app.test_request_context():
+    print(url_for('hello_world'))
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
